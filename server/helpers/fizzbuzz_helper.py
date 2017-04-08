00ae9e6f7a7011ae339b29bc273db9089dbf8f9e
Cristian <cristian@cristianpoll.com>
2017-04-08 14:12:07 -0400

    add fizzbuzz helper

---
+++ server/helpers/fizzbuzz_helper.py
@@ -0,0 +1,22 @@
+__MAX_ALLOWED_FIZZBUZZ_NUMBER = 100000
+
+def get_fizzbuzz_range(number):
+    """
+    Get an array of fizzbuzz values from 1 to number (inclusive)
+    or empty array if the number is invalid
+    """
+
+    if number < 1 or number > __MAX_ALLOWED_FIZZBUZZ_NUMBER:
+        return []
+
+    return map(get_fizzbuzz_value, range(1, number))
+
+def get_fizzbuzz_value(number):
+    if number % 15 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number

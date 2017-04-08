471d7730f8d16d0b2b41ea6c0dfec60b59719785
Cristian <cristian@cristianpoll.com>
2017-04-08 15:31:16 -0400

    add logic to fizzbuzz route

--- server/routes/fizzbuzz/fizzbuzz_route.py
+++ server/routes/fizzbuzz/fizzbuzz_route.py
@@ -1,6 +1,15 @@
-from flask import Blueprint
+import json
+from flask import Blueprint, jsonify
+from server.helpers.fizzbuzz_helper import get_fizzbuzz_range
 
 FIZZBUZZ_BLUEPRINT = Blueprint('fizzbuzz', __name__)
 @FIZZBUZZ_BLUEPRINT.route('/fizzbuzz/<number>')
 def fizzbuzz_route(number):
-    return number
+    try:
+        number = int(number)
+    except ValueError:
+        response = jsonify({'message': 'input must take the form of /fizzbuzz/<number>'})
+        response.status_code = 400
+        return response
+
+    return json.dumps({'fizzbuzz': get_fizzbuzz_range(number)})

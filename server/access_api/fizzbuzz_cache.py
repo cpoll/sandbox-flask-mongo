92a33911b744002c631fdda317b928b37ac573f7
Cristian Poll <cristian@cristianpoll.com>
2017-04-08 20:08:17 -0400

    add fizzbuzz caching using mongodb (#3)
    * add class to cache fizzbuzz values
    
    * add caching to fizzbuzz

---
+++ server/access_api/fizzbuzz_cache.py
@@ -0,0 +1,28 @@
+"""
+    Methods used to store and retrieve permanently cached values
+    for the 'computationally expensive'* fizzbuzz algorithm.
+    *obviously not actually true or a good idea.
+"""
+
+from pymongo import MongoClient
+
+class Fizzbuzz_Cache:
+    def __init__(self):
+        mongo_client = MongoClient('mongodb://localhost:27017/')
+        db = mongo_client.test_database
+        self.cache = db.fizzbuzz_cache
+
+    def get_fizzbuzz_value(self, number):
+        fizzbuzz_entry = self.cache.find_one({"number": number})
+        if fizzbuzz_entry and fizzbuzz_entry["fizzbuzz_result"]:
+            return fizzbuzz_entry["fizzbuzz_result"]
+        else:
+            return False
+
+    def cache_fizzbuzz_value(self, number, fizzbuzz_result):
+        fizzbuzz_entry = {
+            "number": number,
+            "fizzbuzz_result": fizzbuzz_result
+        }
+        cache_id = self.cache.insert_one(fizzbuzz_entry)
+        return cache_id
--- server/routes/fizzbuzz/fizzbuzz_route.py
+++ server/routes/fizzbuzz/fizzbuzz_route.py
@@ -1,6 +1,7 @@
 import json
 from flask import Blueprint, jsonify
 from server.helpers.fizzbuzz_helper import get_fizzbuzz_range
+from server.access_api.fizzbuzz_cache import Fizzbuzz_Cache
 
 FIZZBUZZ_BLUEPRINT = Blueprint('fizzbuzz', __name__)
 @FIZZBUZZ_BLUEPRINT.route('/fizzbuzz/<number>')
@@ -12,4 +13,12 @@ def fizzbuzz_route(number):
         response.status_code = 400
         return response
 
-    return json.dumps({'fizzbuzz': get_fizzbuzz_range(number)})
+    fizzbuzz_cache = Fizzbuzz_Cache()
+    fizzbuzz_value = fizzbuzz_cache.get_fizzbuzz_value(number)
+
+    if not fizzbuzz_value:
+        print("fizzbuzz cache miss")
+        fizzbuzz_value = get_fizzbuzz_range(number)
+        fizzbuzz_cache.cache_fizzbuzz_value(number, fizzbuzz_value)
+
+    return json.dumps({'fizzbuzz': fizzbuzz_value})
